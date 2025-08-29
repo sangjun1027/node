43d9d15595eb02efb18edc366138c10718165d6f
zzun <sangjun1027@gmail.com>
2025-08-29 18:11:40 +0900

    0828 node

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -6,14 +6,33 @@
 const express = require("express");
 const parser = require("body-parser");
 const sql = require("./sql"); //index파일은 굳이 안적어줘도됨
+const prodSql = require("./sql/sql"); //객체 안에는 {productList:{query:''}, productDeyail:{query:''}}이 있다
+const cors = require("cors");
+
+// console.log(prodSql["imageList"].query); //query가 가지고있는 값 중에서 ...
 
 const app = express();
 app.use(parser.urlencoded()); // x-www-form-yrlencoded, / key : value형식
 app.use(parser.json()); // json으로 data를 넘기겠다는 의미
+app.use(cors());
 
 // '라우팅'정보 중 최상위
 app.get("/", (req, resp) => {
   resp.send("/ 실행");
+});
+
+// 상품쿼리
+app.post("/api/:alias", async (req, resp) => {
+  let search = prodSql[req.params.alias].query;
+  let param = req.body.param; //요청정보에 body에 요청정보를 넘긴다, ex)[{product_id:9, type:1, path:test.jpg}] 이런형식으로 들어감
+  // param:[2]
+  try {
+    let result = await sql.execute(search, param); // query문을 변수'search'에 넣음 , 매개값으로 param
+    resp.json(result);
+  } catch (err) {
+    // console.log(err);
+    resp.json({ retCode: "Error" });
+  }
 });
 
 // 고객 목록 정보 보기
--- dev01.sql
+++ dev01.sql
@@ -3,7 +3,67 @@ show databases;
 -- dev라는 database를 사용하겠다 --
 use dev;	
 
-show tables;
+show tables;		-- table을 보여달라 --
 
 select *
 from customers;
+
+-- 아래부터 교재저자 sql db 연결 된 상태 --
+select *
+from t_category;
+
+select *
+from t_image;
+-- where type = 3 9번상품, ype=2, path:sample.jpg
+
+select *
+from t_product;
+
+select *
+from t_user;
+-- insert into t_user set ? on duplicate key update ? --
+-- ↑중복키가 있으면 update, 없으면 insert --
+insert into 	t_user 
+set 			email='hong@example.com',
+				type=1,
+                nickname='홍길동'
+on duplicate key update type=2,nickname='홍길동';
+
+select *
+from t_seller;
+
+-- productList --
+select  	t1.*
+	      , t2.path
+          , t3.category1, t3.category2, t3.category3 
+from 		t_product t1, 
+			t_image t2, 
+			t_category t3
+where 		t1.id = t2.product_id 
+and 		t2.type = 1 
+and 		t1.category_id = t3.id;
+
+-- productList2 --
+select 		t3.*, t4.path 
+from 		(select t1.*, t2.category1, t2.category2, t2.category3 
+from 		t_product t1, 
+			t_category t2
+where 		t1.category_id = t2.id) t3
+left join 	(select  * 
+			   from  t_image 
+               where type=1) t4
+				on 	 t3.id = t4.product_id;
+                
+-- productDetail --
+select 		t1.*, 
+			t2.path, 
+            t3.category1, 
+            t3.category2, 
+            t3.category3 
+from 		t_product t1, 
+			t_image t2, 
+			t_category t3
+where 		t1.id = 1 
+and 		t1.id = t2.product_id 
+and 		t2.type = 3 
+and 		t1.category_id = t3.id;
